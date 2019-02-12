471f2b2f8522080a61dac3c137200fa197cce4e7
Branislav Vukelic <branislav.vukelic@gmail.com>
2019-02-12 11:39:28 +0100

    Change to PHPinfo page

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-require_once("libs/simplepie.php");
-require_once("classes/importer.php");
-
-$xmlData = file_get_contents("tests/fixtures/rss.xml");
-
-$objImporter = new Importer();
-$arrArticles = $objImporter->importArticles($xmlData);
-var_dump($arrArticles);
+// Show all information, defaults to INFO_ALL
+phpinfo();
 
 ?>
